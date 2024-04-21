5914e45f0ddd9198c70e3c6380a3280364ce0471
Vlad Tavrel <vladislav.tavrel@gmail.com>
2024-04-21 19:24:22 +0200

    Task1.1 solution

---
+++ Task1.1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input two integers");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+            for (int i = a; i <= b; i++)
+            {
+                int j = i;
+                while (j > 11)
+                {
+                    if (j % 12 == 10) counter++;
+                    j /= 12;
+                }
+                if (counter == 2) Console.Write(i + " ");
+                counter = 0;
+            }
+        }
+    }
+}
