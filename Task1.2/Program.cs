38b4f9122d67cd72636783d9b4f22d90b07d2316
Vlad Tavrel <vladislav.tavrel@gmail.com>
2024-04-21 20:08:40 +0200

    Task1.2 solution

---
+++ Task1.2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input 9 digits");
+            var digitInput = Console.ReadLine();
+            int multiple = 10;
+            int sum = 0;
+            foreach (var digit in digitInput.ToCharArray())
+            {
+                sum += multiple-- * (int)Char.GetNumericValue(digit);
+            }
+            int checkDigit = 11 - sum % 11;
+            if (checkDigit == 10) Console.WriteLine("Check digit id X");
+            else Console.WriteLine($"Check digit is {checkDigit}");
+        }
+    }
+}
