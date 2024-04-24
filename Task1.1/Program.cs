4513198ae2113bd6ec20e8355f02d7959302be20
Vlad Tavrel <94539512+Vomchick@users.noreply.github.com>
2024-04-24 17:55:16 +0200

    Task1.1 solution (#1)
    * Task1.1 solution
    
    * Corrected program so it can work with any int numbers in any order
    
    * Corrected code style on if statements
    
    * More accurate if's

---
+++ Task1.1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input two integers");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int bigger = Math.Max(a, b);
+            int smaller = Math.Min(a, b);
+            int counter = 0;
+            for (int i = smaller; i <= bigger; i++)
+            {
+                int j = i;
+                while (Math.Abs(j) > 11)
+                {
+                    if (Math.Abs(j % 12) == 10)
+                    {
+                        counter++;
+                    }
+                    j /= 12;
+                }
+                if (counter == 2)
+                {
+                    Console.Write(i + " ");
+                }
+                counter = 0;
+            }
+        }
+    }
+}
