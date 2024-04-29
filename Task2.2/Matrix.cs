63fb222a57365caf69ab02676d90cfb6ad7e162b
Vlad Tavrel <vladislav.tavrel@gmail.com>
2024-04-29 19:55:22 +0200

    Task2.2 solution

---
+++ Task2.2/Matrix.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2._2
+{
+    public class Matrix
+    {
+        int[] _elements;
+        public int Size { get; }
+
+        public Matrix(params int[] elements)
+        {
+            if (elements == null)
+            {
+                Size = 0;
+                _elements = [];
+            }
+            else
+            {
+                Size = elements.Length;
+                _elements = elements;
+            }
+
+        }
+
+        public int this[int i, int j]
+        {
+            get 
+            {
+                if (IndexCheck(i,j) && i == j)
+                {
+                    return _elements[i];
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                if (IndexCheck(i,j) && i == j)
+                {
+                    _elements[i] = value;
+                }
+            }
+        }
+        bool IndexCheck(int i, int j)
+        {
+            if(i < Size && i  >= 0 && j < Size && j >= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int Track()
+        {
+            int sum = 0;
+            foreach (int i in _elements) 
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            var matrix = obj as Matrix;
+            if (matrix != null && matrix.Size == Size)
+            {
+                for (int i = 0; i < Size; i++)
+                {
+                    if (matrix[i,i] != _elements[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for(int i = 0; i < Size; i++)
+            {
+                for(int j = 0; j < Size; j++)
+                {
+                    sb.Append(this[i,j] + " ");
+                }
+                sb.Append("\n");
+            }
+            return sb.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 0;
+            foreach(var element in _elements)
+            {
+                hash += element.GetHashCode();
+            }
+            return hash + Size.GetHashCode();
+        }
+    }
+}
---
+++ Task2.2/MatrixExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2._2
+{
+    public static class MatrixExtension
+    {
+        public static Matrix AddMatrix(this Matrix a, Matrix b)
+        {
+            var elements = new int[Math.Max(a.Size, b.Size)];
+            for (int i = 0; i < elements.Length; i++)
+            {
+                elements[i] = a[i,i] + b[i,i];
+            }
+            return new Matrix(elements);
+        }
+    }
+}
