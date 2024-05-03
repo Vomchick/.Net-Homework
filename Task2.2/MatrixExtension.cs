207aa61fc6e8d8128abe42594f2f186da8f8e013
Vlad Tavrel <vladislav.tavrel@gmail.com>
2024-05-03 15:14:51 +0200

    Changed logic in extension method

--- Task2.2/MatrixExtension.cs
+++ Task2.2/MatrixExtension.cs
@@ -5,9 +5,22 @@ public static class MatrixExtension
     public static Matrix AddMatrix(this Matrix a, Matrix b)
     {
         var elements = new int[Math.Max(a.Size, b.Size)];
+        var minSize = Math.Min(a.Size, b.Size);
+        var isASmaller = a.Size < b.Size;
         for (int i = 0; i < elements.Length; i++)
         {
-            elements[i] = a[i,i] + b[i,i];
+            if (i < minSize)
+            {
+                elements[i] = a[i, i] + b[i, i];
+            }
+            else if (isASmaller)
+            {
+                elements[i] = b[i, i];    
+            }
+            else
+            {
+                elements[i] = a[i, i];
+            }
         }
         return new Matrix(elements);
     }
