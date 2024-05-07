0b5af8097b7bf059d7ab6d4c96314edae7c0ae8e
Vlad Tavrel <94539512+Vomchick@users.noreply.github.com>
2024-05-07 18:53:19 +0200

    Task2.1 solution (#4)
    * Task2.1 solution
    
    * Divided asserts in tests to different methods, corrected double comparison, ermoved unused usings, corrected method naming

---
+++ PointTest/PointTest.cs
@@ -0,0 +1,129 @@
+using Task2._1;
+
+namespace PointTest
+{
+    [TestClass]
+    public class PointTests
+    {
+        [TestMethod]
+        public void PointMassNegativeValue()
+        {
+            var point1 = new Task2._1.Point()
+            {
+                X = 1,
+                Y = 2,
+                Z = 3,
+                Mass = -4f
+            };
+
+            Assert.IsTrue(point1.Mass == 0f);
+        }
+
+        [TestMethod]
+        public void PointIsZero()
+        {
+            var point1 = new Task2._1.Point()
+            {
+                X = 0,
+                Y = 0,
+                Z = 0,
+                Mass = -4f
+            };
+
+            Assert.IsTrue(point1.IsZero());
+        }
+
+        [TestMethod]
+        public void PointIsNotZero()
+        {
+            var point1 = new Task2._1.Point()
+            {
+                X = 0,
+                Y = 0,
+                Z = 1,
+                Mass = -4f
+            };
+
+            Assert.IsFalse(point1.IsZero());
+        }
+
+        [TestMethod]
+        public void PointDistanceToZeroPoint()
+        {
+            var point1 = new Point()
+            {
+                X = 1,
+                Y = 2,
+                Z = 3,
+                Mass = -4f
+            };
+
+            var point2 = new Point()
+            {
+                X = 0,
+                Y = 0,
+                Z = 0,
+                Mass = 5f
+            };
+            Assert.IsTrue(point1.Distance(point2) == 3.7416573867739413);
+        }
+
+        [TestMethod]
+        public void PointDistanceToItself()
+        {
+            var point1 = new Point()
+            {
+                X = 1,
+                Y = 2,
+                Z = 3,
+                Mass = -4f
+            };
+
+            Assert.IsTrue(point1.Distance(point1) == 0.0);
+        }
+
+        [TestMethod]
+        public void PointDistanceToOtherPoint()
+        {
+            var point1 = new Point()
+            {
+                X = 1,
+                Y = 2,
+                Z = 3,
+                Mass = -4f
+            };
+
+            var point2 = new Point()
+            {
+                X = 7,
+                Y = 1,
+                Z = 3,
+                Mass = 5f
+            };
+
+            Assert.IsTrue((point1.Distance(point2) - 6.082762530298219) < Double.Epsilon);
+        }
+
+        [TestMethod]
+        public void PointDistanceToOtherPointSwapped()
+        {
+            var point1 = new Point()
+            {
+                X = 1,
+                Y = 2,
+                Z = 3,
+                Mass = -4f
+            };
+
+            var point2 = new Point()
+            {
+                X = 7,
+                Y = 1,
+                Z = 3,
+                Mass = 5f
+            };
+
+            Assert.IsTrue((point2.Distance(point1) - 6.082762530298219) < Double.Epsilon);
+        }
+    }
+}
