42ea74e9307edf0ac6652a3ce732acc5e151d185
Vlad Tavrel <vladislav.tavrel@gmail.com>
2024-05-07 21:11:54 +0200

    Task3.1 solved but without test class

---
+++ QueueTask3_1/IQueue.cs
@@ -0,0 +1,10 @@
+﻿namespace QueueTask3_1;
+
+public interface IQueue<T> : ICloneable
+{
+    public void Enqueue(T value);
+
+    public void Dequeue();
+
+    public bool IsEmpty();
+}
---
+++ QueueTask3_1/Queue.cs
@@ -0,0 +1,81 @@
+﻿namespace QueueTask3_1;
+
+public class Queue<T> : IQueue<T>
+{
+    private QueueElement<T>? _head;
+    private QueueElement<T>? _tail;
+
+    public Queue(T value)
+    {
+        _head = new QueueElement<T> { Value = value };
+        _tail = _head;
+    }
+    public Queue() { }
+
+    private Queue(QueueElement<T>? head, QueueElement<T>? tail)
+    {
+        _head = head;
+        _tail = tail;
+    }
+
+    public object Clone() 
+    {
+        if(_head == null)
+        {
+            return new Queue<T>(_head, _tail);
+        }
+
+        var newValuesFromHead = (QueueElement<T>)_head?.Clone();
+        var iterator = newValuesFromHead;
+        QueueElement<T> newTail = null;
+        while (iterator != null)
+        {
+            if(iterator.Next == null)
+            {
+                newTail = iterator;
+            }
+            iterator = iterator.Next;
+        }
+        return new Queue<T>(newValuesFromHead, newTail);
+    } 
+
+    public void Dequeue()
+    {
+        try
+        {
+            _head = _head.Next;
+            if (_head == null) 
+            { 
+                _tail = null;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+
+    public void Enqueue(T value)
+    {
+        try
+        {
+            if (_tail != null)
+            {
+                _tail.Next = new QueueElement<T> { Value = value };
+                _tail = _tail.Next;
+            }
+            else
+            {
+                _head = new QueueElement<T> { Value = value };
+                _tail = _head;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+
+    public bool IsEmpty() => _head == null;
+}
+
